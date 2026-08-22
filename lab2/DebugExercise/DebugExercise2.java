75b8ed731a39cad37a217ce895effd239295e562
CLiun <chengnan.liu39@gmail.com>
2026-08-23 00:57:06 +0900

    fix func max

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -1,5 +1,9 @@
 package DebugExercise;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
 /**
  * Exercise to showcase the step over button.
  * Code adapted from https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
@@ -7,15 +11,23 @@ package DebugExercise;
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
+        int w = (a - b) >> 31;
         /* If you're stepping into this function, click the
            step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
+        int z = ~(a - b) >> 31;
 
         int max = b & w | a & z;
         return max;
     }
 
+    @Test
+    public void testMax() {
+        int a, b;
+        a = 10;
+        b = 5;
+        int bigger = max(a, b);
+        assertEquals(10, bigger);
+    }
 
     /** Returns the sum of a and b. Do not step into this function. */
     public static int add(int a, int b) {
