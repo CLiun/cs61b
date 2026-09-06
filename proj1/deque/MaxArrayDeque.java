d704b94286639b93ec659c4e7cdf800ae7373957
CLiun <chengnan.liu39@gmail.com>
2026-09-07 03:56:29 +0900

    Done deque part

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -32,4 +32,11 @@ public class MaxArrayDeque<T> extends ArrayDeque<T>{
         return tempMax;
     }
 
+    public static void main(String[] args) {
+        MaxArrayDeque<Integer> maxArray = new MaxArrayDeque<>(new MyComparator());
+        maxArray.addFirst(3);
+        maxArray.addFirst(88);
+        maxArray.addFirst(99);
+    }
+
 }
